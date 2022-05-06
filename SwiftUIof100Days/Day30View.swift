ad0d1c44c3d2e112e3fc4dac860bd42ad599cb50
Ray <ray00178@gmail.com>
2022-05-07 00:37:39 +0800

    - add(Project): Practice Day 30~31 lesson

--- SwiftUIof100Days/Day30View.swift
+++ SwiftUIof100Days/Day30View.swift
@@ -13,6 +13,10 @@ struct Day30View: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
         NavigationView {
             List {
@@ -34,6 +38,14 @@ struct Day30View: View {
             .onSubmit {
                 addNewWord()
             }
+            .onAppear(perform: {
+                startGame()
+            })
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) {}
+            } message: {
+                Text(errorMessage)
+            }
         }
     }
     
@@ -42,11 +54,74 @@ struct Day30View: View {
         
         guard answer.count > 0 else { return }
         
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original!")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
         withAnimation {
             useWords.insert(answer, at: 0)
         }
         
         newWord = ""
+    }
+    
+    func startGame() {
+        guard let fileURL = Bundle.main.url(forResource: "start", withExtension: "txt"),
+              let startWords = try? String(contentsOf: fileURL)
+        else {
+            fatalError("Could not load start.txt from bundle.")
+        }
+        
+        let allWords = startWords.components(separatedBy: "\n")
+        
+        rootWord = allWords.randomElement() ?? "silkworm"
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !useWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let index = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: index)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word,
+                                                            range: range,
+                                                            startingAt: 0,
+                                                            wrap: false,
+                                                            language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
---
+++ SwiftUIof100Days/Day32View.swift
@@ -0,0 +1,20 @@
+//
+//  Day32View.swift
+//  SwiftUIof100Days
+//
+//  Created by Ray on 2022/5/7.
+//
+
+import SwiftUI
+
+struct Day32View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Day32View_Previews: PreviewProvider {
+    static var previews: some View {
+        Day32View()
+    }
+}
