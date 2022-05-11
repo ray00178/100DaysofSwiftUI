29a6ea67bcf8cf53d724d474def76ac6fbd5c140
Ray <ray00178@gmail.com>
2022-05-11 23:38:28 +0800

    - add(Project): Practice Day 33 lesson of Showing and hiding views with transitions & Building custom transitions using ViewModifier

--- SwiftUIof100Days/Day33View.swift
+++ SwiftUIof100Days/Day33View.swift
@@ -8,34 +8,31 @@
 import SwiftUI
 
 struct Day33View: View {
-    let letters = Array("Hello SwiftUI")
     
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
+    @State private var isShowingRed = false
     
     var body: some View {
         //TapMeButton()
         //DragView()
-        HStack(spacing: 0) {
-            ForEach(0 ..< letters.count) {
-                Text(String(letters[$0]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? .blue : .red)
-                    .offset(dragAmount)
-                    .animation(.default.delay(Double($0) / 20), value: dragAmount)
+        //AnimatingView()
+        //ShowingView()
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged({ value in
-                    dragAmount = value.translation
-                })
-                .onEnded({ value in
-                    dragAmount = .zero
-                    enabled.toggle()
-                })
-        )
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
     }
 }
 
@@ -81,8 +78,91 @@ struct DragView: View {
     }
 }
 
+struct AnimatingView: View {
+    
+    let letters = Array("Hello SwiftUI")
+    
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0 ..< letters.count) {
+                Text(String(letters[$0]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.default.delay(Double($0) / 20), value: dragAmount)
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged({ value in
+                    dragAmount = value.translation
+                })
+                .onEnded({ value in
+                    dragAmount = .zero
+                    enabled.toggle()
+                })
+        )
+    }
+    
+}
+
+struct ShowingView: View {
+    
+    @State private var isShowingRed = false
+    
+    var body: some View {
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    //.transition(.scale)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
+            
+        }
+    }
+}
+
 struct Day33View_Previews: PreviewProvider {
     static var previews: some View {
         Day33View()
     }
+}
+
+// MARK: - Custom modifier
+
+struct CornerRotateModifier: ViewModifier {
+    
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+// MARK: - Extenison
+
+extension AnyTransition {
+    
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+    
 }
