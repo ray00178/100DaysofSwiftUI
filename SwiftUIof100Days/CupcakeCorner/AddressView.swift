f6799ae47aa55ef494a220a87c71d7da33c16262
Ray <ray00178@gmail.com>
2022-06-09 22:41:42 +0800

    - add(Project): Practice Day50 lesson

---
+++ SwiftUIof100Days/CupcakeCorner/AddressView.swift
@@ -0,0 +1,41 @@
+//
+//  AddressView.swift
+//  SwiftUIof100Days
+//
+//  Created by Ray on 2022/6/9.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    
+    @ObservedObject var order = Order()
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink {
+                    CheckoutView(order: order)
+                } label: {
+                    Text("Check out")
+                }
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddressView()
+    }
+}
