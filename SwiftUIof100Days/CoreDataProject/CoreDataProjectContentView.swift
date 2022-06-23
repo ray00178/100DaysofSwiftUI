b35ed2f200715605f3dd08ccef9e0616bdc42331
Ray <ray00178@gmail.com>
2022-06-23 23:31:04 +0800

    - add(Project): Practice Day58 lesson of Filtering @FetchRequest using NSPredicate

--- SwiftUIof100Days/CoreDataProject/CoreDataProjectContentView.swift
+++ SwiftUIof100Days/CoreDataProject/CoreDataProjectContentView.swift
@@ -14,27 +14,59 @@ struct CoreDataProjectContentView: View {
 
   @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
 
+  // "universe == 'Star Wars'"
+  // "name < %@", "F"
+  // "universe IN %@", ["Aliens", "Firefly", "Star Trek"]
+  // "NOT name BEGINSWITH[c] %@", "e"
+  @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e")) var ships: FetchedResults<Ship>
+
   var body: some View {
     VStack(spacing: 8.0) {
-      List(wizards, id: \.self) { wizard in
-        Text(wizard.name ?? "Unknown Title")
+      // For Ships
+      List(ships, id: \.self) { ship in
+        Text(ship.name ?? "Unknown Name")
       }
 
-      Button("Add") {
-        let wizard = Wizard(context: moc)
-        wizard.name = "Harry Potter"
-        
+      Button("Add Example") {
+        let ship1 = Ship(context: moc)
+        ship1.name = "Enterprise"
+        ship1.universe = "Star Trek"
+
+        let ship2 = Ship(context: moc)
+        ship2.name = "Defiant"
+        ship2.universe = "Star Trek"
+
+        let ship3 = Ship(context: moc)
+        ship3.name = "Millennium Falcon"
+        ship3.universe = "Star Wars"
+
+        let ship4 = Ship(context: moc)
+        ship4.name = "Executor"
+        ship4.universe = "Star Wars"
+
+        try? moc.save()
       }
-      .padding(8)
-      
-      Button("Save") {
-        do {
-          try moc.save()
-        } catch {
-          print(error.localizedDescription)
-        }
-      }
-      .padding(8)
+
+      // For wizards
+      /* List(wizards, id: \.self) { wizard in
+         Text(wizard.name ?? "Unknown Title")
+       }
+
+       Button("Add") {
+         let wizard = Wizard(context: moc)
+         wizard.name = "Harry Potter"
+
+       }
+       .padding(8)
+
+       Button("Save") {
+         do {
+           try moc.save()
+         } catch {
+           print(error.localizedDescription)
+         }
+       }
+       .padding(8) */
     }
   }
 }
