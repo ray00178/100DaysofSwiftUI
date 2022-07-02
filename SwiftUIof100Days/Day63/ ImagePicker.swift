1df0a27e29d9d5eef5b8c19e89b56752d43065e3
Ray <ray00178@gmail.com>
2022-07-03 00:15:52 +0800

    - add(Project): Practice Day63 lesson of Using coordinators to manage SwiftUI view controllers

--- SwiftUIof100Days/Day63/ ImagePicker.swift
+++ SwiftUIof100Days/Day63/ ImagePicker.swift
@@ -9,16 +9,45 @@ import PhotosUI
 import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
-    
+  
+  @Binding var image: UIImage?
+  
   func makeUIViewController(context: Context) -> some UIViewController {
     var config = PHPickerConfiguration()
     config.filter = .images
     
     let picker = PHPickerViewController(configuration: config)
+    picker.delegate = context.coordinator
     return picker
   }
   
   func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
     
+  }
+  
+  func makeCoordinator() -> Coordinator {
+    return Coordinator(self)
+  }
+  
+  // MARK: - Inner Class
+  class Coordinator: NSObject, PHPickerViewControllerDelegate {
+    
+    var parent: ImagePicker
+    
+    init(_ parent: ImagePicker) {
+      self.parent = parent
+    }
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+      picker.dismiss(animated: true)
+      
+      guard let provider = results.first?.itemProvider else { return }
+      
+      if provider.canLoadObject(ofClass: UIImage.self) {
+        provider.loadObject(ofClass: UIImage.self) { data, _ in
+          self.parent.image = data as? UIImage
+        }
+      }
+    }
   }
 }
--- SwiftUIof100Days/Day63/Day63View.swift
+++ SwiftUIof100Days/Day63/Day63View.swift
@@ -5,9 +5,9 @@
 //  Created by Ray on 2022/6/30.
 //
 
-import SwiftUI
 import CoreImage
 import CoreImage.CIFilterBuiltins
+import SwiftUI
 
 // MARK: - Day63View
 
@@ -15,62 +15,71 @@ struct Day63View: View {
   
   @State private var image: Image?
   @State private var showingImagePicker = false
-  
+  @State private var inputImage: UIImage?
+
   var body: some View {
-    
     // Wrapping a UIViewController in a SwiftUI view
     VStack(spacing: 20.0) {
       image?
         .resizable()
         .scaledToFit()
-      
+
       Button("Select Image") {
         showingImagePicker = true
       }
+      .textCase(.uppercase)
       .padding()
     }
     .sheet(isPresented: $showingImagePicker) {
-      ImagePicker()
+      ImagePicker(image: $inputImage)
     }
-    
+    .onChange(of: inputImage, perform: { _ in
+      loadImageFromPicker()
+    })
+
     // Integrating Core Image with SwiftUI
-    /*VStack {
-      image?
-        .resizable()
-        .scaledToFit()
-    }
-    .onAppear {
-      loadImage()
-    }*/
+    /* VStack {
+       image?
+         .resizable()
+         .scaledToFit()
+     }
+     .onAppear {
+       loadImage()
+     } */
   }
-  
+
   func loadImage() {
     guard let beginImage = UIImage(named: "apollo17") else { return }
-    
+
     let inputImage = CIImage(image: beginImage)
-    
+
     let context = CIContext()
     let currentFilter = CIFilter.pixellate()
     let inputKeys = currentFilter.inputKeys
-    
+
     currentFilter.inputImage = inputImage
-    
+
     if inputKeys.contains(kCIInputIntensityKey) {
-      //currentFilter.intensity = 1.0
+      // currentFilter.intensity = 1.0
     }
-    
+
     if inputKeys.contains(kCIInputScaleKey) {
       currentFilter.scale = 15
     }
-    
+
     guard let outputImage = currentFilter.outputImage,
           let cgimg = context.createCGImage(outputImage, from: outputImage.extent)
     else {
       return
     }
-    
+
     let uiImage = UIImage(cgImage: cgimg)
     image = Image(uiImage: uiImage)
+  }
+
+  func loadImageFromPicker() {
+    guard let inputImage = inputImage else { return }
+    image = Image(uiImage: inputImage)
   }
 }
 
