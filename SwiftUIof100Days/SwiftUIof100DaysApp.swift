31009dc4d34d98c5d177cc1f48b78900c9f9a635
Ray <ray00178@gmail.com>
2022-07-11 23:12:39 +0800

    - add(Project): Practice Day69 lesson of Integrating MapKit with SwiftUI

--- SwiftUIof100Days/SwiftUIof100DaysApp.swift
+++ SwiftUIof100Days/SwiftUIof100DaysApp.swift
@@ -24,7 +24,7 @@ struct SwiftUIof100DaysApp: App {
       /*CoreDataProjectContentView()
         .environment(\.managedObjectContext, dataController2.container.viewContext)*/
       
-      Day68View()
+      Day69View()
     }
   }
 }
