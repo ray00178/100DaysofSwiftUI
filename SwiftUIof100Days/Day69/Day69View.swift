33db31927438b31e4c73bea42b203a323e42b186
Ray <ray00178@gmail.com>
2022-07-13 23:04:47 +0800

    - add(Project): Practice Day69 lesson of Using Touch ID and Face ID with SwiftUI

--- SwiftUIof100Days/Day69/Day69View.swift
+++ SwiftUIof100Days/Day69/Day69View.swift
@@ -5,6 +5,7 @@
 //  Created by Ray on 2022/7/11.
 //
 
+import LocalAuthentication
 import MapKit
 import SwiftUI
 
@@ -12,7 +13,9 @@ import SwiftUI
 
 struct Day69View: View {
   @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
-
+  
+  @State private var isUnlocked = false
+  
   let locations: [Location] = [
     Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
     Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076)),
@@ -20,22 +23,58 @@ struct Day69View: View {
 
   var body: some View {
     NavigationView {
-      Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-        MapAnnotation(coordinate: location.coordinate) {
-          NavigationLink {
-            Text(location.name)
-              .foregroundColor(.blue)
-              .padding()
-              .font(.system(size: 20, design: .monospaced))
-          } label: {
-            Circle()
-              .stroke(.red, lineWidth: 3)
-              .frame(width: 44, height: 33)
+      ZStack(alignment: .bottom) {
+        Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+          MapAnnotation(coordinate: location.coordinate) {
+            NavigationLink {
+              Text(location.name)
+                .foregroundColor(.blue)
+                .padding()
+                .font(.system(size: 20, design: .monospaced))
+            } label: {
+              Circle()
+                .stroke(.red, lineWidth: 3)
+                .frame(width: 44, height: 33)
+            }
           }
+        }
+        
+        if isUnlocked {
+          Text("Unlocked")
+            .padding(20)
+            .background(.white)
+            .cornerRadius(4)
+        } else {
+          Text("Locked")
+            .padding(20)
+            .background(.white)
+            .cornerRadius(4)
         }
       }
       .navigationTitle("London Explorer")
       .ignoresSafeArea(edges: .bottom)
+      .onAppear(perform: authenticate)
+    }
+  }
+
+  func authenticate() {
+    let context = LAContext()
+    var error: NSError?
+
+    if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+      let reason = "We need to unlock your data."
+
+      context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, _ in
+        isUnlocked = success
+        
+        if success {
+          // authenticated successfully
+        } else {
+          // there was a problem
+        }
+      }
+    } else {
+      // no biometrics
     }
   }
 }
