8fa9233fb2d17c281c340b9dfcbd12f1423a887f
Ray <ray00178@gmail.com>
2022-07-20 21:52:44 +0800

    - add(Project): Practice Dya72 lession of Locking our UI behind Face ID

--- SwiftUIof100Days/Day70/Day70View.swift
+++ SwiftUIof100Days/Day70/Day70View.swift
@@ -15,48 +15,58 @@ struct Day70View: View {
 
   var body: some View {
     ZStack {
-      Map(coordinateRegion: $viewModel.mapRegion,
-          annotationItems: viewModel.locations) { location in
-        MapAnnotation(coordinate: location.coordinate) {
-          VStack {
-            Image(systemName: "star.circle")
-              .resizable()
-              .foregroundColor(.red)
-              .frame(width: 44, height: 44)
-              .background(.white)
-              .clipShape(Circle())
+      if viewModel.isUnlocked {
+        Map(coordinateRegion: $viewModel.mapRegion,
+            annotationItems: viewModel.locations) { location in
+          MapAnnotation(coordinate: location.coordinate) {
+            VStack {
+              Image(systemName: "star.circle")
+                .resizable()
+                .foregroundColor(.red)
+                .frame(width: 44, height: 44)
+                .background(.white)
+                .clipShape(Circle())
 
-            Text(location.name)
-              .fixedSize()
-          }
-          .onTapGesture {
-            viewModel.selectedPlace = location
+              Text(location.name)
+                .fixedSize()
+            }
+            .onTapGesture {
+              viewModel.selectedPlace = location
+            }
           }
         }
-      }
-      .ignoresSafeArea()
+        .ignoresSafeArea()
 
-      Circle()
-        .fill(.blue)
-        .opacity(0.3)
-        .frame(width: 32, height: 32)
+        Circle()
+          .fill(.blue)
+          .opacity(0.3)
+          .frame(width: 32, height: 32)
 
-      VStack {
-        Spacer()
-        HStack {
+        VStack {
           Spacer()
-          Button {
-            viewModel.addLocation()
-          } label: {
-            Image(systemName: "plus")
+          HStack {
+            Spacer()
+            Button {
+              viewModel.addLocation()
+            } label: {
+              Image(systemName: "plus")
+            }
+            .padding()
+            .background(.black.opacity(0.75))
+            .foregroundColor(.white)
+            .font(.title)
+            .clipShape(Circle())
+            .padding(.trailing)
           }
-          .padding()
-          .background(.black.opacity(0.75))
-          .foregroundColor(.white)
-          .font(.title)
-          .clipShape(Circle())
-          .padding(.trailing)
         }
+      } else {
+        Button("Unlock Places") {
+          viewModel.authenticate()
+        }
+        .padding()
+        .background(.blue)
+        .foregroundColor(.white)
+        .clipShape(Capsule())
       }
     }
     .sheet(item: $viewModel.selectedPlace) { place in
--- SwiftUIof100Days/Day71/ContentView-ViewModel.swift
+++ SwiftUIof100Days/Day71/ContentView-ViewModel.swift
@@ -5,15 +5,15 @@
 //  Created by Ray on 2022/7/19.
 //
 
+import LocalAuthentication
 import MapKit
 
 extension Day70View {
-  
   @MainActor class ViewModel: ObservableObject {
-    
     @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
     @Published private(set) var locations: [Day70View.Location]
     @Published var selectedPlace: Day70View.Location?
+    @Published var isUnlocked: Bool = false
 
     let savePath = FileManager.documentsDictionary.appendingPathComponent("SavedPlaces")
 
@@ -29,7 +29,7 @@ extension Day70View {
     func addLocation() {
       let newLocation = Day70View.Location(id: UUID(), name: "New location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
       locations.append(newLocation)
-      
+
       save()
     }
 
@@ -38,7 +38,7 @@ extension Day70View {
 
       if let index = locations.firstIndex(of: selectedPlace) {
         locations[index] = location
-        
+
         save()
       }
     }
@@ -49,6 +49,37 @@ extension Day70View {
         try data.write(to: savePath, options: [.atomic, .completeFileProtection])
       } catch {
         print("Unable to save data.")
+      }
+    }
+
+    func authenticate() {
+      let context = LAContext()
+      var error: NSError?
+
+      if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+        let reason = "Please authenticate yourself to unlock your places."
+
+        context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,
+                               localizedReason: reason)
+        { success, _ in
+          if success {
+            /*Task {
+              await MainActor.run(body: {
+                self.isUnlocked = true
+              })
+            }*/
+            
+            // or
+            
+            Task { @MainActor in
+              self.isUnlocked = true
+            }
+          } else {
+            // error
+          }
+        }
+      } else {
+        // no biometrics
       }
     }
   }
