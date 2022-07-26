fc9b359bc33af81e8310deee5f3c95c5661a742d
Ray <ray00178@gmail.com>
2022-07-26 22:06:51 +0800

    - add(Project): Practice Day75 ~ Day77 lesson

--- SwiftUIof100Days/Bookworm/RatingView.swift
+++ SwiftUIof100Days/Bookworm/RatingView.swift
@@ -27,17 +27,34 @@ struct RatingView: View {
       if label.isEmpty == false {
         Text(label)
       }
-      
+
       ForEach(1 ... maxRating, id: \.self) { number in
         image(number: number)
           .foregroundColor(number > rating ? offColor : onColor)
           .onTapGesture {
             rating = number
           }
+          /*.accessibilityLabel("\(number == 1 ? "1 star" : "\(number) stars")")
+          .accessibilityRemoveTraits(.isImage)
+          .accessibilityAddTraits(number > rating ? .isButton : [.isButton, .isSelected])*/
+          // or
+          .accessibilityElement()
+          .accessibilityLabel(label)
+          .accessibilityValue(rating == 1 ? "1 star" : "\(number) stars")
+          .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+              if rating < maxRating { rating += 1 }
+            case .decrement:
+              if rating > 1 { rating -= 1 }
+            default:
+              break
+            }
+          }
       }
     }
   }
-  
+
   func image(number: Int) -> Image {
     if number > maxRating {
       return offImage ?? onImage
--- SwiftUIof100Days/Day30/Day30View.swift
+++ SwiftUIof100Days/Day30/Day30View.swift
@@ -7,126 +7,131 @@
 
 import SwiftUI
 
+// MARK: - Day30View
+
 struct Day30View: View {
-    
-    @State private var useWords = [String]()
-    @State private var rootWord = ""
-    @State private var newWord = ""
-    
-    @State private var errorTitle = ""
-    @State private var errorMessage = ""
-    @State private var showingError = false
-    
-    var body: some View {
-        NavigationView {
-            List {
-                Section {
-                    TextField("Enter your word", text: $newWord)
-                        .autocapitalization(.none)
-                }
-                
-                Section {
-                    ForEach(useWords, id: \.self) { word in
-                        HStack {
-                            Image(systemName: "\(word.count).circle")
-                            Text(word)
-                        }
-                    }
-                }
+  @State private var useWords = [String]()
+  @State private var rootWord = ""
+  @State private var newWord = ""
+
+  @State private var errorTitle = ""
+  @State private var errorMessage = ""
+  @State private var showingError = false
+
+  var body: some View {
+    NavigationView {
+      List {
+        Section {
+          TextField("Enter your word", text: $newWord)
+            .autocapitalization(.none)
+        }
+
+        Section {
+          ForEach(useWords, id: \.self) { word in
+            HStack {
+              Image(systemName: "\(word.count).circle")
+              Text(word)
             }
-            .navigationTitle(rootWord)
-            .onSubmit {
-                addNewWord()
-            }
-            .onAppear(perform: {
-                startGame()
-            })
-            .alert(errorTitle, isPresented: $showingError) {
-                Button("OK", role: .cancel) {}
-            } message: {
-                Text(errorMessage)
-            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("\(word), \(word.count) letters")
+          }
         }
+      }
+      .navigationTitle(rootWord)
+      .onSubmit {
+        addNewWord()
+      }
+      .onAppear(perform: {
+        startGame()
+      })
+      .alert(errorTitle, isPresented: $showingError) {
+        Button("OK", role: .cancel) {}
+      } message: {
+        Text(errorMessage)
+      }
     }
-    
-    func addNewWord() {
-        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        
-        guard answer.count > 0 else { return }
-        
-        guard isOriginal(word: answer) else {
-            wordError(title: "Word used already", message: "Be more original!")
-            return
-        }
-        
-        guard isPossible(word: answer) else {
-            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
-            return
-        }
-        
-        guard isReal(word: answer) else {
-            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
-            return
-        }
-        
-        withAnimation {
-            useWords.insert(answer, at: 0)
-        }
-        
-        newWord = ""
+  }
+
+  func addNewWord() {
+    let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+    guard answer.count > 0 else { return }
+
+    guard isOriginal(word: answer) else {
+      wordError(title: "Word used already", message: "Be more original!")
+      return
     }
-    
-    func startGame() {
-        guard let fileURL = Bundle.main.url(forResource: "start", withExtension: "txt"),
-              let startWords = try? String(contentsOf: fileURL)
-        else {
-            fatalError("Could not load start.txt from bundle.")
-        }
-        
-        let allWords = startWords.components(separatedBy: "\n")
-        
-        rootWord = allWords.randomElement() ?? "silkworm"
+
+    guard isPossible(word: answer) else {
+      wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+      return
     }
-    
-    func isOriginal(word: String) -> Bool {
-        !useWords.contains(word)
+
+    guard isReal(word: answer) else {
+      wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+      return
     }
-    
-    func isPossible(word: String) -> Bool {
-        var tempWord = rootWord
-        
-        for letter in word {
-            if let index = tempWord.firstIndex(of: letter) {
-                tempWord.remove(at: index)
-            } else {
-                return false
-            }
-        }
-        
-        return true
+
+    withAnimation {
+      useWords.insert(answer, at: 0)
     }
-    
-    func isReal(word: String) -> Bool {
-        let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word,
-                                                            range: range,
-                                                            startingAt: 0,
-                                                            wrap: false,
-                                                            language: "en")
-        
-        return misspelledRange.location == NSNotFound
+
+    newWord = ""
+  }
+
+  func startGame() {
+    guard let fileURL = Bundle.main.url(forResource: "start", withExtension: "txt"),
+          let startWords = try? String(contentsOf: fileURL)
+    else {
+      fatalError("Could not load start.txt from bundle.")
     }
-    
-    func wordError(title: String, message: String) {
-        errorTitle = title
-        errorMessage = message
-        showingError = true
+
+    let allWords = startWords.components(separatedBy: "\n")
+
+    rootWord = allWords.randomElement() ?? "silkworm"
+  }
+
+  func isOriginal(word: String) -> Bool {
+    !useWords.contains(word)
+  }
+
+  func isPossible(word: String) -> Bool {
+    var tempWord = rootWord
+
+    for letter in word {
+      if let index = tempWord.firstIndex(of: letter) {
+        tempWord.remove(at: index)
+      } else {
+        return false
+      }
     }
+
+    return true
+  }
+
+  func isReal(word: String) -> Bool {
+    let checker = UITextChecker()
+    let range = NSRange(location: 0, length: word.utf16.count)
+    let misspelledRange = checker.rangeOfMisspelledWord(in: word,
+                                                        range: range,
+                                                        startingAt: 0,
+                                                        wrap: false,
+                                                        language: "en")
+
+    return misspelledRange.location == NSNotFound
+  }
+
+  func wordError(title: String, message: String) {
+    errorTitle = title
+    errorMessage = message
+    showingError = true
+  }
 }
 
+// MARK: - Day30_Previews
+
 struct Day30_Previews: PreviewProvider {
-    static var previews: some View {
-        Day30View()
-    }
+  static var previews: some View {
+    Day30View()
+  }
 }
