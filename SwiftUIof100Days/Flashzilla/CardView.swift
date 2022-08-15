c3645a7488d4e3400128e97b5900cb98d4dc32f1
Ray <ray00178@gmail.com>
2022-08-15 22:02:01 +0800

    - add(Project): Practice Day89 lesson

--- SwiftUIof100Days/Flashzilla/CardView.swift
+++ SwiftUIof100Days/Flashzilla/CardView.swift
@@ -10,18 +10,29 @@ import SwiftUI
 // MARK: - CardView
 
 struct CardView: View {
+  @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+
   @State private var isShowingAnswer = false
 
   @State private var offset = CGSize.zero
 
   let card: Card
 
-  var removal: (() -> Void)? = nil
-  
+  var removal: (() -> Void)?
+
   var body: some View {
     ZStack {
       RoundedRectangle(cornerRadius: 25.0, style: .circular)
-        .fill(.white)
+        .fill(
+          accessibilityDifferentiateWithoutColor ?
+            .white :
+            .white.opacity(1 - Double(abs(offset.width / 50))))
+        .background(
+          accessibilityDifferentiateWithoutColor ?
+            nil :
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+              .fill(offset.width > 0 ? .green : .red)
+        )
         .shadow(radius: 10)
 
       VStack {
--- SwiftUIof100Days/Flashzilla/Day88View.swift
+++ SwiftUIof100Days/Flashzilla/Day88View.swift
@@ -10,7 +10,15 @@ import SwiftUI
 // MARK: - Day88View
 
 struct Day88View: View {
+  @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+
+  @Environment(\.scenePhase) var scenePhase
+
   @State private var cards: [Card] = Array(repeating: Card.example, count: 10)
+  @State private var timeRemaining = 100
+  @State private var isActive = true
+
+  let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
   var body: some View {
     ZStack {
@@ -18,7 +26,15 @@ struct Day88View: View {
         .resizable()
         .ignoresSafeArea()
 
-      VStack {
+      VStack(spacing: 20) {
+        Text("Time: \(timeRemaining)")
+          .font(.largeTitle)
+          .foregroundColor(.white)
+          .padding(.horizontal, 20)
+          .padding(.vertical, 5)
+          .background(.black.opacity(0.75))
+          .clipShape(Capsule())
+
         ZStack {
           ForEach(0 ..< cards.count, id: \.self) { index in
             CardView(card: cards[index]) {
@@ -29,12 +45,70 @@ struct Day88View: View {
             .statck(at: index, total: cards.count)
           }
         }
+        .allowsHitTesting(timeRemaining > 0)
+
+        if cards.isEmpty {
+          Button("Start Agent", action: resetCards)
+            .padding()
+            .background(.white)
+            .foregroundColor(.black)
+            .clipShape(Capsule())
+        }
+      }
+
+      if accessibilityDifferentiateWithoutColor {
+        VStack {
+          Spacer()
+
+          HStack {
+            Image(systemName: "xmark.circle")
+              .padding()
+              .background(.black.opacity(0.7))
+              .clipShape(Circle())
+
+            Spacer()
+
+            Image(systemName: "checkmark.circle")
+              .padding()
+              .background(.black.opacity(0.7))
+              .clipShape(Circle())
+          }
+          .foregroundColor(.white)
+          .font(.largeTitle)
+          .padding()
+        }
+      }
+    }.onReceive(timer) { _ in
+      guard isActive else { return }
+
+      if timeRemaining > 0 {
+        timeRemaining -= 1
+      }
+    }
+    .onChange(of: scenePhase) { newValue in
+      switch newValue {
+      case .active:
+        if cards.isEmpty == false {
+          isActive = true
+        }
+      default:
+        isActive = false
       }
     }
   }
 
   func removeCard(at index: Int) {
     cards.remove(at: index)
+
+    if cards.isEmpty {
+      isActive = false
+    }
+  }
+
+  func resetCards() {
+    cards = Array(repeating: Card.example, count: 10)
+    timeRemaining = 100
+    isActive = true
   }
 }
 
