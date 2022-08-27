47a45c67d00de93ae7aaff6cd32c005fad4159c2
Ray <ray00178@gmail.com>
2022-08-27 21:50:41 +0800

    - add(Project): Practice Day96 lesson

---
+++ SwiftUIof100Days/SnowSeeker/Day96User.swift
@@ -0,0 +1,14 @@
+//
+//  Day96User.swift
+//  SwiftUIof100Days
+//
+//  Created by Ray on 2022/8/27.
+//
+
+import Foundation
+
+struct Day96User: Identifiable {
+  
+  var id: String = "Taylor Swift"
+  
+}
--- SwiftUIof100Days/SnowSeeker/Day96View.swift
+++ SwiftUIof100Days/SnowSeeker/Day96View.swift
@@ -10,23 +10,93 @@ import SwiftUI
 // MARK: - Day96View
 
 struct Day96View: View {
-  var body: some View {
-    NavigationView {
-      NavigationLink {
-        if #available(iOS 16.0, *) {
-          Text("New secondary")
-            .toolbar(.hidden)
-        } else {
-          Text("New secondary")
-            .navigationBarHidden(true)
-        }
-      } label: {
-        Text("Hello, World!")
-      }
-      .navigationTitle("Primary")
+  @State private var selectedUser: Day96User?
+  @State private var isShowingUser = false
+  @State private var searchText: String = ""
+
+  @Environment(\.horizontalSizeClass) var horizontalSizeClass
+
+  let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
+
+  var filteredNames: [String] {
+    if searchText.isEmpty {
+      return allNames
+    } else {
+      return allNames.filter { $0.localizedCaseInsensitiveContains(searchText) }
     }
-    
-    Text("Secondary")
+  }
+
+  var body: some View {
+    // Making a SwiftUI view searchable
+    NavigationView {
+      List(filteredNames, id: \.self) { name in
+        Text(name)
+      }
+      .searchable(text: $searchText, prompt: "Input some key word...")
+      .navigationTitle("Searching")
+    }
+
+    // Using groups as transparent layout containers
+    /* if horizontalSizeClass == .compact {
+       VStack(spacing: 8.0) {
+         ForEach(0 ..< 10) { _ in
+           DemoView()
+         }
+       }
+     } else {
+       HStack(spacing: 8.0) {
+         ForEach(0 ..< 10) { _ in
+           DemoView()
+         }
+       }
+     } */
+
+    // Using alert() and sheet() with optionals
+    /* Text("Hi Friends~")
+     .onTapGesture {
+       selectedUser = Day96User()
+       isShowingUser = true
+     }
+     .alert("Welcome", isPresented: $isShowingUser, presenting: selectedUser) { user in
+       Button(user.id) {}
+     }
+     .alert("Welcome", isPresented: $isShowingUser) {}
+     .sheet(isPresented: $isShowingUser) {
+       Text(selectedUser?.id ?? "Unknown")
+     }
+     .sheet(item: $selectedUser) { user in
+       Text("\(user.id)")
+     } */
+
+    // Working with two side by side views in SwiftUI
+    /* NavigationView {
+       NavigationLink {
+         if #available(iOS 16.0, *) {
+           Text("New secondary")
+             .toolbar(.hidden)
+         } else {
+           Text("New secondary")
+             .navigationBarHidden(true)
+         }
+       } label: {
+         Text("Hello, World!")
+       }
+       .navigationTitle("Primary")
+     }
+
+     Text("Secondary") */
+  }
+
+  @ViewBuilder
+  func DemoView() -> some View {
+    HStack(spacing: 12.0) {
+      Image(systemName: "moon.stars.fill")
+      Text("Moon")
+        .font(.subheadline)
+        .foregroundColor(.gray)
+    }
+    .font(.largeTitle)
+    .foregroundColor(.yellow)
   }
 }
 
